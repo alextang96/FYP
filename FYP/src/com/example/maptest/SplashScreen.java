20f36cd23ab6bb0219bf50a39a90664a04370dd5
alextang962 <alextang_ht@yahoo.com.hk>
2013-12-14 11:19:51 +0800

    Testing the commit in alextang962

--- FYP/src/com/example/maptest/SplashScreen.java
+++ FYP/src/com/example/maptest/SplashScreen.java
@@ -37,10 +37,7 @@ public class SplashScreen extends Activity {
 				synchronized (this) {
 					// Wait given period of time or exit on touch
 					wait(1000);
-					Log.e("hello world", "i am jerry");
-					Log.e("hello world2", "i am jerry2");
-					Log.e("Hello World", "I am Alex");
-					Log.e("123","321");
+					Log.e("Hello World", "I am Alex2");
 				}
 			} catch (InterruptedException ex) {
 			}
